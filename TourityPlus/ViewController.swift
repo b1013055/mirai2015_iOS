0402efae37eae6c4d5e798f6fdf1642e56108f0d
武安裕輔 <b1013055@gmail.com>
2015-08-06 14:37:15 +0900

    first commit

---
+++ TourityPlus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TourityPlus
+//
+//  Created by 武安裕輔 on 2015/08/06.
+//  Copyright (c) 2015年 武安裕輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
